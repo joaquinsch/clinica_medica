5acad5d45304646638e46f09ab78709166f4dacc
Joaquin Schanzenbach <joacohao1@gmail.com>
2025-01-23 17:42:48 -0300

    agrego endpoint para buscar paciente por id

--- src/main/java/com/example/clinica_medica/controller/PacienteController.java
+++ src/main/java/com/example/clinica_medica/controller/PacienteController.java
@@ -1,8 +1,12 @@
 package com.example.clinica_medica.controller;
 
+import java.util.NoSuchElementException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +31,16 @@ public class PacienteController {
 			return ResponseEntity.status(HttpStatus.CREATED).body("Se ha creado");
 		} catch (Exception e) {
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Algo salió mal");
+		}
+	}
+	
+	@GetMapping("/buscar/{id_paciente}")
+	public ResponseEntity<?> buscarPaciente(@PathVariable Long id_paciente){
+		try {
+			Paciente buscado = pacienteService.buscarPaciente(id_paciente);
+			return ResponseEntity.status(HttpStatus.ACCEPTED).body(buscado);
+		} catch(NoSuchElementException e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se encontró el paciente");
 		}
 	}
 
