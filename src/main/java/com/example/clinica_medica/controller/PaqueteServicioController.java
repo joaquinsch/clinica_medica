07e884c584960af6e674b25eab80e457ea782aca
Joaquin Schanzenbach <joacohao1@gmail.com>
2025-01-24 17:01:34 -0300

    agrego endpoint para buscar paquete

--- src/main/java/com/example/clinica_medica/controller/PaqueteServicioController.java
+++ src/main/java/com/example/clinica_medica/controller/PaqueteServicioController.java
@@ -1,8 +1,12 @@
 package com.example.clinica_medica.controller;
 
+import java.util.NoSuchElementException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,4 +32,13 @@ public class PaqueteServicioController {
 		}
 	}
 
+	@GetMapping("/buscar/{id_paquete}")
+	public ResponseEntity<?> buscarPaqueteServicio(@PathVariable Long id_paquete) {
+		try {
+			PaqueteServicio paqueteBuscado = paqueteServicioService.buscarPaqueteServicio(id_paquete);
+			return ResponseEntity.status(HttpStatus.OK).body(paqueteBuscado);
+		} catch (NoSuchElementException e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se encontró el paquete");
+		}
+	}
 }
