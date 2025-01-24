a56ae455100c0d47f5166b972bd628102b2e824f
Joaquin Schanzenbach <joacohao1@gmail.com>
2025-01-24 01:03:15 -0300

    agrego endpoint para editar turno

--- src/main/java/com/example/clinica_medica/controller/TurnoController.java
+++ src/main/java/com/example/clinica_medica/controller/TurnoController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,5 +44,16 @@ public class TurnoController {
         }
     }
     
+    @PutMapping("/editar")
+    public ResponseEntity<?> editarTurno(@RequestBody Turno turno) {
+        try {
+            Turno turnoEditado = turnoService.editarMedico(turno);
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body(turnoEditado);
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se encontró el turno");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Algo salió mal");
+        }
+    }
 
 }
