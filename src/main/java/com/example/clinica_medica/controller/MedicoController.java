c9ad42432c4e2f1ab827f0f902ebdcade785e742
Joaquin Schanzenbach <joacohao1@gmail.com>
2025-01-24 00:08:14 -0300

    agrego endpoint para eliminar medico

--- src/main/java/com/example/clinica_medica/controller/MedicoController.java
+++ src/main/java/com/example/clinica_medica/controller/MedicoController.java
@@ -5,6 +5,7 @@ import java.util.NoSuchElementException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -50,6 +51,18 @@ public class MedicoController {
 			Medico aEditar = medicoService.editarMedico(medico);
 			return ResponseEntity.status(HttpStatus.ACCEPTED).body(aEditar);
 		} catch (NoSuchElementException e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se encontró el médico");
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Algo salió mal");
+		}
+	}
+
+	@DeleteMapping("/eliminar/{id_medico}")
+	public ResponseEntity<?> eliminarPaciente(@PathVariable Long id_medico){
+		try {
+			medicoService.eliminarMedico(id_medico);
+			return ResponseEntity.status(HttpStatus.ACCEPTED).body("Se ha eliminado");
+		} catch(NoSuchElementException e) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se encontró el médico");
 		} catch (Exception e) {
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Algo salió mal");
