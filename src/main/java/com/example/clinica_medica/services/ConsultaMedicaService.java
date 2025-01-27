be46092b73cbc6b329f6cd2fcd1f256bf42453d3
Joaquin Schanzenbach <joacohao1@gmail.com>
2025-01-27 19:19:34 -0300

    agrego funcionalidad para que el monto de una consulta se calcule a partir de los precios del paquete o servicio elegidos

--- src/main/java/com/example/clinica_medica/services/ConsultaMedicaService.java
+++ src/main/java/com/example/clinica_medica/services/ConsultaMedicaService.java
@@ -4,7 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.clinica_medica.model.ConsultaMedica;
-
+import com.example.clinica_medica.model.PaqueteServicio;
+import com.example.clinica_medica.model.ServicioMedico;
 import com.example.clinica_medica.repository.ConsultaMedicaRepository;
 
 @Service
@@ -16,12 +17,28 @@ public class ConsultaMedicaService {
 	@Autowired
 	private TurnoService turnoService;
 
+	@Autowired
+	private ServicioMedicoService servicioMedicoService;
+
+	@Autowired
+	private PaqueteServicioService paqueteService;
+
 	public ConsultaMedica guardarConsultaMedica(ConsultaMedica consulta) {
 		if (!turnoService.hayTurnoDisponible(consulta.getUn_medico(), consulta.getFecha_consulta(),
 				consulta.getHora_consulta())) {
 			throw new IllegalArgumentException("No hay turnos disponibles en el horario o fecha elegidos");
 		}
 		if (consulta.getUn_servicio_medico() == null ^ consulta.getUn_paquete_servicio() == null) {
+			if (consulta.getUn_servicio_medico() != null) {
+				ServicioMedico servicioMedico = servicioMedicoService
+						.buscarServicioMedico(consulta.getUn_servicio_medico().getCodigo_servicio());
+				consulta.setMonto_total(servicioMedico.getPrecio());
+			} else {
+				PaqueteServicio paqueteServicio = paqueteService
+						.buscarPaqueteServicio(consulta.getUn_paquete_servicio().getCodigo_paquete());
+				consulta.setMonto_total(paqueteServicio.getPrecio_paquete());
+			}
+
 			return consultaMedicaRepo.save(consulta);
 		} else {
 			throw new IllegalArgumentException(
