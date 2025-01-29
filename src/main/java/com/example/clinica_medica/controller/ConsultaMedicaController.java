e12a0602043ffe12aa582eef493da5bfde272121
Joaquin Schanzenbach <joacohao1@gmail.com>
2025-01-28 23:55:35 -0300

    agrego endpoint buscar consulta medica por id

--- src/main/java/com/example/clinica_medica/controller/ConsultaMedicaController.java
+++ src/main/java/com/example/clinica_medica/controller/ConsultaMedicaController.java
@@ -3,6 +3,8 @@ package com.example.clinica_medica.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +22,9 @@ public class ConsultaMedicaController {
 	 * 
 	 * - TERMINAR CRUD CONSULTAMEDICA
 	 * - VERIFICAR QUE SI UN TURNO YA FUE USADO, NO PUEDA VOLVER A USARSE.
+	 * -REFACTOREAR HAYTURNODISPONIBLE PARA Q LLAME A SERVICES SOLO
+	 * 
+	 * **********************************
 	 * -REVISAR RELACION N a N DE PAQUETES Y SERVICIOS (NO SE SI ES NECESARIO GUARDAR
 	 * ESTO) 
 	 * - PREGUNTAR POR CONSULTA MEDICA CON PAQUETE. YA QUE SE ESTÁ ASUMIENDO
@@ -28,7 +33,7 @@ public class ConsultaMedicaController {
 	 * - FECHA Y HORA DE UNA CONSULTA CON PAQUETE, SE ESTÁ ASUMIENDO QUE
 	 * EN ESA FECHA Y HORA DE LA CONSULTA SE HACE TODo LO QUE TRAE EL PAQUETE
 	 * DENTRO...
-	 * -REFACTOREAR HAYTURNODISPONIBLE PARA Q LLAME A SERVICES SOLO
+	 * 
 	 * 
 	 */
 
@@ -40,5 +45,11 @@ public class ConsultaMedicaController {
 		
 		consultaMedicaService.guardarConsultaMedica(consulta);
 		return ResponseEntity.status(HttpStatus.CREATED).body(consulta);
+	}
+	
+	@GetMapping("/buscar/{id_consulta_medica}")
+	public ResponseEntity<ConsultaMedica> buscarConsultaMedica(@PathVariable Long id_consulta_medica){
+		ConsultaMedica consultaBuscada = consultaMedicaService.buscarConsultaMedica(id_consulta_medica);
+		return new ResponseEntity<ConsultaMedica>(consultaBuscada, HttpStatus.OK);
 	}
 }
