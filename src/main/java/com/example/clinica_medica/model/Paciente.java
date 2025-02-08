108db66d2e17e532169fcb4dce1c21616dbc609f
Joaquin Schanzenbach <joacohao1@gmail.com>
2025-02-08 01:17:30 -0300

    valido obra social paciente

--- src/main/java/com/example/clinica_medica/model/Paciente.java
+++ src/main/java/com/example/clinica_medica/model/Paciente.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -20,6 +21,7 @@ public class Paciente extends Usuario {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id_paciente;
 	
+	@NotNull(message = "Debes colocar si tiene obra social o no")
 	private Boolean tiene_obra_social;
 
 	@OneToMany(mappedBy = "un_paciente")
