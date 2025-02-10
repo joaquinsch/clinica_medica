24199977ac0266269953c143cc3776cadb961978
Joaquin Schanzenbach <joacohao1@gmail.com>
2025-02-10 00:56:46 -0300

    agrego tests para controller de paciente

--- src/test/java/com/example/clinica_medica/tests/PacienteControllerTests.java
+++ src/test/java/com/example/clinica_medica/tests/PacienteControllerTests.java
@@ -1,9 +1,11 @@
 package com.example.clinica_medica.tests;
 
 import static org.mockito.Mockito.times;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withAccepted;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.time.LocalDate;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,13 +17,13 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.example.clinica_medica.controller.PacienteController;
 import com.example.clinica_medica.model.Paciente;
 import com.example.clinica_medica.services.PacienteService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @ExtendWith(MockitoExtension.class)
 public class PacienteControllerTests {
@@ -42,14 +44,17 @@ public class PacienteControllerTests {
     public void setUp() {
     	this.mockMvc = MockMvcBuilders.standaloneSetup(pacienteController).build();
     	objectMapper = new ObjectMapper();
+    	// necesario para usar LocalDate
+    	objectMapper.registerModule(new JavaTimeModule());
         paciente = new Paciente();
         paciente.setId_paciente(1L);
         paciente.setNombre("Juan");
         paciente.setApellido("Pérez");
         paciente.setDni("12345678");
         paciente.setEmail("juan.perez@example.com");
-        //paciente.setFecha_nac(LocalDate.of(1990, 5, 15));
+        paciente.setFecha_nac(LocalDate.of(1990, 5, 15));
         paciente.setTelefono("123456789");
+        paciente.setDireccion("Calle falsa 123");
         paciente.setTiene_obra_social(true);
     }
 	
@@ -64,7 +69,13 @@ public class PacienteControllerTests {
 				.andExpect(status().isCreated())
 				.andExpect(jsonPath("$.id_paciente").value(1))
 				.andExpect(jsonPath("$.nombre").value("Juan"))
-                .andExpect(jsonPath("$.apellido").value("Pérez"));
+                .andExpect(jsonPath("$.apellido").value("Pérez"))
+                .andExpect(jsonPath("$.dni").value("12345678"))
+                .andExpect(jsonPath("$.email").value("juan.perez@example.com"))
+				.andExpect(jsonPath("$.fecha_nac").value("15/05/1990"))
+				.andExpect(jsonPath("$.telefono").value("123456789"))
+				.andExpect(jsonPath("$.direccion").value("Calle falsa 123"))
+				.andExpect(jsonPath("$.tiene_obra_social").value(true));
 	}
 	
 	@Test
@@ -92,5 +103,33 @@ public class PacienteControllerTests {
 				.andExpect(jsonPath("$.apellido").value("Pérez"));
 		
 		Mockito.verify(pacienteService, times(1)).buscarPaciente(paciente.getId_paciente());
+	}
+	
+	@Test
+	public void deberiaEditarUnPaciente() throws Exception{
+		paciente.setId_paciente(1L);
+		paciente.setNombre("Carlos");
+		paciente.setApellido("González");
+		paciente.setDni("12312312");
+		paciente.setEmail("carlos.gonzalez@example.com");
+		paciente.setFecha_nac(LocalDate.of(1982, 4, 20));
+		paciente.setTelefono("1166885544");
+		paciente.setDireccion("Calle falsa 124");
+		paciente.setTiene_obra_social(false);
+		String pacienteEditadoJson = objectMapper.writeValueAsString(paciente);
+
+		Mockito.when(pacienteService.editarPaciente(Mockito.any(Paciente.class))).thenReturn(paciente);
+		mockMvc.perform(MockMvcRequestBuilders.put("/api/pacientes/editar")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(pacienteEditadoJson)
+				).andExpect(jsonPath("$.id_paciente").value(1L))
+				.andExpect(jsonPath("$.nombre").value("Carlos"))
+				.andExpect(jsonPath("$.apellido").value("González"))
+                .andExpect(jsonPath("$.dni").value("12312312"))
+                .andExpect(jsonPath("$.email").value("carlos.gonzalez@example.com"))
+				.andExpect(jsonPath("$.fecha_nac").value("20/04/1982"))
+				.andExpect(jsonPath("$.telefono").value("1166885544"))
+				.andExpect(jsonPath("$.direccion").value("Calle falsa 124"))
+				.andExpect(jsonPath("$.tiene_obra_social").value(false));
 	}
 }
